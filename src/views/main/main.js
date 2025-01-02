890451b23f2a15bea19bfb123b1b0f0bea35ed3c
EBUSYGIN <e.busygin2003@gmail.com>
2025-01-02 08:58:15 +0300

    add state

--- src/views/main/main.js
+++ src/views/main/main.js
@@ -1,15 +1,32 @@
 import { AbstractView } from '../../common/view';
+import onChange from 'on-change';
 
 export class MainView extends AbstractView {
-  constructor() {
+  state = {
+    list: [],
+    loading: false,
+    searchQuery: undefined,
+    offset: 0
+  };
+
+  constructor(appState) {
     super();
     this.setTitle('Поиск книг');
+    this.appState = appState;
+    this.appState = onChange(this.appState, this.appStateHook.bind(this));
+  }
+
+  appStateHook(path, state, newState) {
+    console.log(path);
+    console.log(state);
+    console.log(newState);
   }
 
   render() {
     this.app.innerHTML = '';
     const main = document.createElement('div');
-    main.innerHTML = 'test';
+    main.innerHTML = `Число книг: ${this.appState.favorites.length}`;
     this.app.append(main);
+    this.appState.favorites.push('');
   }
 }
