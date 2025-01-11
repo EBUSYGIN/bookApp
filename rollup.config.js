e1050dcb0a6093974a2a57d6d903647cc8977a1e
EBUSYGIN <e.busygin2003@gmail.com>
2025-01-11 22:27:56 +0300

    fix

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import postcss from 'rollup-plugin-postcss';
-import copy from 'rollup-plugin-copy';
+import html from '@rollup/plugin-html';
 
 export default {
   input: 'src/app.js',
@@ -13,10 +13,30 @@ export default {
     postcss({
       modules: true
     }),
-    copy({
-      targets: [
-        { src: 'index.html', dest: 'dist' } // Копируем index.html в dist
-      ]
+    html({
+      template: () => `
+        <!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <script src="./app.js" type="module"></script>
+    <link rel="stylesheet" href="./bundle.css" />
+    
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap"
+      rel="stylesheet"
+    />
+  </head>
+  <body>
+    <div id="root"></div>
+  </body>
+</html>
+
+      `
     })
   ]
 };
