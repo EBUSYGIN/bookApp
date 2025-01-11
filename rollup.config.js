d2895a5649b58b1ed45e37ba40bb134090d4b0e2
EBUSYGIN <e.busygin2003@gmail.com>
2025-01-11 22:12:48 +0300

    fix

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,6 @@
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import postcss from 'rollup-plugin-postcss';
+import html from '@rollup/plugin-html';
 
 export default {
   input: 'src/app.js',
@@ -11,6 +12,22 @@ export default {
     nodeResolve(),
     postcss({
       modules: true
+    }),
+    html({
+      template: () => `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <title>Book App</title>
+        </head>
+        <body>
+          <div id="root"></div>
+          <script src="bundle.js"></script>
+        </body>
+        </html>
+      `
     })
   ]
 };
