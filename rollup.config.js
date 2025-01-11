b9af85f31ab9fc7cc4fa18f1a80dace2baf7ddf6
EBUSYGIN <e.busygin2003@gmail.com>
2025-01-11 22:14:03 +0300

    fix

--- rollup.config.js
+++ rollup.config.js
@@ -15,18 +15,26 @@ export default {
     }),
     html({
       template: () => `
-        <!DOCTYPE html>
-        <html lang="en">
-        <head>
-          <meta charset="UTF-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <title>Book App</title>
-        </head>
-        <body>
-          <div id="root"></div>
-          <script src="bundle.js"></script>
-        </body>
-        </html>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+    <script src="./app.js" type="module"></script>
+    <link rel="stylesheet" href="./bundle.css" />
+    <link rel="stylesheet" href="./static/global.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap"
+      rel="stylesheet"
+    />
+  </head>
+  <body>
+    <div id="root"></div>
+  </body>
+</html>
       `
     })
   ]
